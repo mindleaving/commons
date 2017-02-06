294a7fbae1425850a6494f373ac22866cc03f8c3
Jan Scholtyssek <jan@janscholtyssek.dk>
2017-02-06 23:08:40 +0100

    HumanGenome SequenceAnalysis commons

---
+++ SIBaseUnit.cs
@@ -0,0 +1,13 @@
+﻿namespace Commons
+{
+    public enum SIBaseUnit
+    {
+        Meter = 0,
+        Kilogram = 1,
+        Second = 2,
+        Ampere = 3,
+        Kelvin = 4,
+        Mole = 5,
+        Candela = 6
+    }
+}
