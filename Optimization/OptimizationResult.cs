eeac1bdc36ff4096b7249fc39bb7cb847e93d3e7
Jan Scholtyssek <jan@janscholtyssek.dk>
2017-05-16 20:27:39 +0200

    Added FilterFuncs

--- Optimization/OptimizationResult.cs
+++ Optimization/OptimizationResult.cs
@@ -15,7 +15,7 @@
 
         public override string ToString()
         {
-            return string.Format("Cost: {0}", Cost);
+            return $"Cost: {Cost}";
         }
     }
 }
---
+++ UnitTests/FilterFuncsTest.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Commons.UnitTests
+{
+    [TestFixture]
+    public class FilterFuncsTest
+    {
+        [Test]
+        public void AverageFilterThrowsExceptionForEvenWindowSize()
+        {
+            var data = new List<double>();
+            Assert.That(() => data.MovingAverage(2).ToList(), Throws.ArgumentException);
+        }
+
+        [Test]
+        public void AverageFilterEmptyArrayReturnsEmpty()
+        {
+            var data = new List<double>();
+            var actual = data.MovingAverage(3).ToList();
+            Assert.That(actual, Is.Empty);
+        }
+
+        [Test]
+        public void AverageFilterSingleValueArrayReturnsThisValue()
+        {
+            var data = new List<double> { 42 };
+            var actual = data.MovingAverage(3).ToList();
+            Assert.That(actual.Count, Is.EqualTo(1));
+            Assert.That(actual[0], Is.EqualTo(42));
+        }
+
+        [Test]
+        public void AverageFilterReturnsInputForWindowSizeOne()
+        {
+            var data = new List<double> { 1, 2, 3, 13, 15 };
+            var actual = data.MovingAverage(1).ToList();
+            Assert.That(actual.Count, Is.EqualTo(data.Count));
+            Assert.That(actual, Is.EqualTo(data));
+        }
+
+        [Test]
+        public void AverageFilterReturnsExceptedMovingAverage()
+        {
+            var data = new List<double> { 1, 2, 3, 13, 15 };
+            var actual = data.MovingAverage(3).ToList();
+            var expected = new List<double> { 4.0/3, 2, 6, 31.0/3, 43.0/3 };
+            Assert.That(actual.Count, Is.EqualTo(data.Count));
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
