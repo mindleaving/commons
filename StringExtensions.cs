99fe0c17ecf9b6dbe7b91afa7491ace407831ecc
Jan Scholtyssek <jan@janscholtyssek.dk>
2017-07-16 01:21:51 +0200

    Using invariant versions of ToUpper and ToLower

--- StringExtensions.cs
+++ StringExtensions.cs
@@ -5,11 +5,11 @@ namespace Commons
 {
     public static class StringExtensions
     {
-        public static string FirstLetterToUpper(this string input)
+        public static string FirstLetterToUpperInvariant(this string input)
         {
             var words = input.Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
             return words
-                .Select(x => x.Substring(0, 1).ToUpper() + x.Substring(1).ToLower())
+                .Select(x => x.Substring(0, 1).ToUpperInvariant() + x.Substring(1).ToLowerInvariant())
                 .Aggregate((a, b) => a + " " + b);
         }
     }
