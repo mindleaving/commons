0670811904fd41a1147ee53fd51d2a484a74e880
Jan Scholtyssek <jan@janscholtyssek.dk>
2018-01-13 17:51:50 +0100

    Added TimePoint and TimeSeries

---
+++ Physics/TimePoint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Commons.Physics
+{
+    public class TimePoint<T>
+    {
+        public TimePoint(DateTime time, T value)
+        {
+            Time = time;
+            Value = value;
+        }
+
+        public DateTime Time { get; }
+        public T Value { get; }
+    }
+}
---
+++ Physics/TimeSeries.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+namespace Commons.Physics
+{
+    public class TimeSeries<T> : List<TimePoint<T>>
+    {
+    }
+}
