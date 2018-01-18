ee33f9a1e3459e4b2e560b74aeb6f826470b4670
Jan Scholtyssek <jan@janscholtyssek.dk>
2018-01-18 23:58:20 +0100

    Added RelayCommand with input parameter

--- Wpf/RelayCommand.cs
+++ Wpf/RelayCommand.cs
@@ -31,4 +31,33 @@ namespace Commons.Wpf
 
         public event EventHandler CanExecuteChanged;
     }
+
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Action<T> action;
+        private readonly Func<bool> canExecute;
+
+        public RelayCommand(Action<T> action)
+        {
+            this.action = action;
+            canExecute = () => true;
+        }
+        public RelayCommand(Action<T> action, Func<bool> canExecute)
+        {
+            this.action = action;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute();
+        }
+
+        public void Execute(object parameter)
+        {
+            action((T)parameter);
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
 }
