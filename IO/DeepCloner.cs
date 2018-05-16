d5b47d52665cd16c969d78769b662e2de4c0b1e5
Jan Scholtyssek <dkjans@chr-hansen.com>
2018-05-16 17:17:41 +0200

    Added DeepCloner

---
+++ IO/DeepCloner.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Runtime.Serialization;
+
+namespace Commons.IO
+{
+    public static class DeepCloner<T>
+    {
+        public static T Clone(T obj)
+        {
+            var serializer = new DataContractSerializer(typeof(T));
+            using (var stream = new MemoryStream())
+            {
+                serializer.WriteObject(stream, obj);
+                stream.Seek(0, SeekOrigin.Begin);
+                return (T) serializer.ReadObject(stream);
+            }
+        }
+    }
+}
