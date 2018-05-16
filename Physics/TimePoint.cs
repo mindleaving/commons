07dce641b81f84f91ebe3dfae3d51d8111e522ba
Jan Scholtyssek <dkjans@chr-hansen.com>
2018-05-16 17:17:27 +0200

    Serialization of TimePoint and TimeSeries

--- Physics/TimePoint.cs
+++ Physics/TimePoint.cs
@@ -1,16 +1,21 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace Commons.Physics
 {
+    [DataContract]
     public class TimePoint<T>
     {
+        public TimePoint() { } // For serialization
         public TimePoint(DateTime time, T value)
         {
             Time = time;
             Value = value;
         }
 
-        public DateTime Time { get; }
-        public T Value { get; }
+        [DataMember]
+        public DateTime Time { get; set; }
+        [DataMember]
+        public T Value { get; set; }
     }
 }
--- Physics/TimeSeries.cs
+++ Physics/TimeSeries.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 namespace Commons.Physics
 {
+    [CollectionDataContract]
     public class TimeSeries<T> : List<TimePoint<T>>
     {
     }
