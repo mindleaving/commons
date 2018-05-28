03b0dfc1f1e7b54cb801eb354c33bf9f7327b76a
Jan Scholtyssek <dkjans@chr-hansen.com>
2018-05-28 14:50:38 +0200

    Added WorkingDirectory for determining path of working directory

---
+++ IO/WorkingDirectory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace Commons.IO
+{
+    public static class WorkingDirectory
+    {
+        public static string GetPath()
+        {
+            var uri = GetUri();
+            var path = Uri.UnescapeDataString(uri.AbsolutePath);
+            return Path.GetDirectoryName(path);
+        }
+
+        public static Uri GetUri()
+        {
+            var codeBase = Assembly.GetExecutingAssembly().CodeBase;
+            return new UriBuilder(codeBase).Uri;
+        }
+    }
+}
