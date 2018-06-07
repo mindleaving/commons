c45df8c0e29c7d2159279c3fc7880466b9637dce
Jan Scholtyssek <jan@janscholtyssek.dk>
2018-06-07 21:11:07 +0200

    Checking overlap of items when constructing clusters in WindowClusterer

--- DataProcessing/WindowClusterer.cs
+++ DataProcessing/WindowClusterer.cs
@@ -24,37 +24,24 @@ namespace Commons.DataProcessing
                 var clusterSize = slidingWindow.Count();
                 clusterSizeTrace.Add(new Point2D(windowPosition, clusterSize));
             }
-            var clusterWindows = new List<ClusterWindowPair<T>>(clusterCount);
+            var clusters = new List<Cluster<T>>(clusterCount);
             var orderedClusterSizes = clusterSizeTrace.OrderByDescending(p => p.Y);
             foreach (var clusterSize in orderedClusterSizes)
             {
                 var windowPosition = clusterSize.X;
                 var clusterCandidateRange = new Range<double>(windowPosition - windowSize / 2, windowPosition + windowSize / 2);
-                // Check that cluster candidate does not overlap with existing cluster
-                // Ideally we check that items do not overlap. For speed we check that windows don't overlap.
-                if(clusterWindows.Any(cluster => cluster.Window.Overlaps(clusterCandidateRange)))
-                    continue;
                 var itemsInRange = orderedItems
                     .SkipWhile(x => valueSelector(x) < clusterCandidateRange.From)
                     .TakeWhile(x => valueSelector(x) <= clusterCandidateRange.To)
                     .ToList();
-                clusterWindows.Add(new ClusterWindowPair<T>(new Cluster<T>(itemsInRange), clusterCandidateRange));
-                if(clusterWindows.Count == clusterCount)
+                // Check that cluster candidate items do not overlap with existing clusters
+                if(clusters.Any(cluster => cluster.Items.Intersect(itemsInRange).Any()))
+                    continue;
+                clusters.Add(new Cluster<T>(itemsInRange));
+                if(clusters.Count == clusterCount)
                     break;
             }
-            return clusterWindows.Select(x => x.Cluster).ToList();
-        }
-
-        private struct ClusterWindowPair<T>
-        {
-            public ClusterWindowPair(Cluster<T> cluster, Range<double> window)
-            {
-                Cluster = cluster;
-                Window = window;
-            }
-
-            public Cluster<T> Cluster { get; }
-            public Range<double> Window { get; }
+            return clusters;
         }
     }
 }
