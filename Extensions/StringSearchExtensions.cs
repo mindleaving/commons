d5d695ce6a8c4044862b453a20ffd1b727a617eb
Jan Scholtyssek <dkjans@chr-hansen.com>
2018-06-22 11:11:22 +0200

    Added StringSearchExtensions with AllIndicesOf-method

---
+++ Extensions/StringSearchExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Commons.Annotations;
+
+namespace Commons.Extensions
+{
+    public static class StringSearchExtensions
+    {
+        public static List<int> AllIndicesOf([NotNull] this string input, string searchValue)
+        {
+            if (input == null) throw new ArgumentNullException(nameof(input));
+            var currentPosition = input.IndexOf(searchValue, StringComparison.InvariantCulture);
+            var indices = new List<int>();
+            while (currentPosition > 0)
+            {
+                indices.Add(currentPosition);
+                currentPosition = input.IndexOf(searchValue, currentPosition+searchValue.Length, StringComparison.InvariantCulture);
+            }
+            return indices;
+        }
+    }
+}
---
+++ UnitTests/Extensions/StringSearchExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using Commons.Extensions;
+using NUnit.Framework;
+
+namespace Commons.UnitTests.Extensions
+{
+    [TestFixture]
+    public class StringSearchExtensionsTest
+    {
+        [Test]
+        public void AllIndicesOfFindsAllOccurences()
+        {
+            var input = "The fox chased the rabbit, while another fox chased an owl";
+            var searchValue = "fox";
+            var expected = new[] {4, 41};
+            var actual = input.AllIndicesOf(searchValue);
+            Assert.That(actual.Count, Is.EqualTo(expected.Length));
+            Assert.That(actual.SequenceEqual(expected));
+        }
+
+        [Test]
+        public void AllIndicesOfThrowsIfInputNull()
+        {
+            string input = null;
+            var searchValue = "fox";
+            Assert.That(() => StringSearchExtensions.AllIndicesOf(input, searchValue), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void AllIndicesOfReturnsEmptyListIfNoMatch()
+        {
+            var input = "The fox chased the rabbit, while another fox chased an owl";
+            var searchValue = "dog";
+            var actual = input.AllIndicesOf(searchValue);
+            Assert.That(actual.Count, Is.EqualTo(0));
+        }
+    }
+}
