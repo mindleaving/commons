4e15d7a5390920c075b3516287e278ea2b148c0b
Jan Scholtyssek <dkjans@chr-hansen.com>
2018-10-16 11:36:03 +0200

    Added ExceptionExtensions

---
+++ Extensions/ExceptionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Commons.Extensions
+{
+    public static class ExceptionExtensions
+    {
+        public static Exception InnermostException(this Exception e)
+        {
+            var innerException = e;
+            while (innerException.InnerException != null)
+            {
+                innerException = innerException.InnerException;
+            }
+            return innerException;
+        }
+    }
+}
