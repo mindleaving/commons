5aa46af70099444674c925950a47fd84883f1bb6
Jan Scholtyssek <jan@janscholtyssek.dk>
2018-11-19 22:07:27 +0100

    Made Point2D readonly

--- Mathematics/Point2D.cs
+++ Mathematics/Point2D.cs
@@ -11,8 +11,8 @@ namespace Commons.Mathematics
             Y = y;
         }
 
-        public double X { get; set; }
-        public double Y { get; set; }
+        public double X { get; }
+        public double Y { get; }
 
         public static Point2D operator +(Point2D point, Vector2D vector)
         {
