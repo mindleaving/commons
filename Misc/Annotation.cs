98809e05ee589b0d317b520a750b4390129a05d0
Jan Scholtyssek <dkjans@chr-hansen.com>
2019-07-10 12:50:46 +0200

    Added AdditionalInformation to Annotation

--- Misc/Annotation.cs
+++ Misc/Annotation.cs
@@ -1,17 +1,24 @@
 ﻿using System.Collections.Generic;
 using Commons.Mathematics;
+using Newtonsoft.Json;
 
 namespace Commons.Misc
 {
     public class Annotation
     {
-        public Annotation(List<Point2D> points, AnnotationShapeType shapeType)
+        [JsonConstructor]
+        public Annotation(
+            List<Point2D> points, 
+            AnnotationShapeType shapeType, 
+            Dictionary<string, string> additionalInformation = null)
         {
             Points = points;
             ShapeType = shapeType;
+            AdditionalInformation = additionalInformation ?? new Dictionary<string, string>();
         }
 
         public AnnotationShapeType ShapeType { get; }
         public List<Point2D> Points { get; }
+        public Dictionary<string,string> AdditionalInformation { get; }
     }
 }
