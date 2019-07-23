5679e34b1b1880a7bb36ed9d41ead7bfe4d88132
Jan Scholtyssek <dkjans@chr-hansen.com>
2019-07-23 16:46:14 +0200

    Added TypeExtensions with IsNumber-extension

---
+++ Extensions/TypeExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Commons.Extensions
+{
+    public static class TypeExtensions
+    {
+        public static bool IsNumber(this object o)
+        {
+            if (ReferenceEquals(o, null))
+                return false;
+            switch (Type.GetTypeCode(o.GetType()))
+            {
+                case TypeCode.Byte:
+                case TypeCode.SByte:
+                case TypeCode.UInt16:
+                case TypeCode.UInt32:
+                case TypeCode.UInt64:
+                case TypeCode.Int16:
+                case TypeCode.Int32:
+                case TypeCode.Int64:
+                case TypeCode.Decimal:
+                case TypeCode.Double:
+                case TypeCode.Single:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
---
+++ UnitTests/Extensions/TypeExtensionsTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Drawing;
+using Commons.Extensions;
+using Commons.Physics;
+using NUnit.Framework;
+
+namespace CommonsTest.Extensions
+{
+    [TestFixture]
+    public class TypeExtensionsTest
+    {
+        private static readonly object[] IsNumberTestCases = {
+            (int) 1, (int) -1, (double) 1.3d, (float) 2.4f, (byte) 0x45,
+        };
+
+        [TestCaseSource(nameof(IsNumberTestCases))]
+        public void IsNumber(object o)
+        {
+            Assert.That(o.IsNumber(), Is.True);
+        }
+
+        private static readonly object[] IsNotANumberTestCases =
+        {
+            null, new UnitValue(Unit.Liter, 2.3), Color.Gray, "3.2", "1"
+        };
+
+        [TestCaseSource(nameof(IsNotANumberTestCases))]
+        public void IsNotANumber(object o)
+        {
+            Assert.That(o.IsNumber(), Is.False);
+        }
+    }
+}
