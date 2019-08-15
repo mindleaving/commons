58dd4efc974316afd2df949af508db68fd22e89b
Jan Scholtyssek <dkjans@chr-hansen.com>
2019-08-15 16:05:20 +0200

    Added Secrets

---
+++ Misc/Secrets.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Commons.Misc
+{
+    public static class Secrets
+    {
+        public static string Get(string secretName)
+        {
+            return Environment.GetEnvironmentVariable(secretName) 
+                   ?? throw new KeyNotFoundException($"Could not find environment variable '{secretName}'");
+        }
+    }
+}
